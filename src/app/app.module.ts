b112f8b5fc37cb9e0e12fc92beeb8a29c421550b
Tô Phở Không Hành <vggff619@gmail.com>
2021-06-04 23:05:38 +0700

    Setup angular firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { GunFormComponent } from './gunform/gunform.component';
 import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,29 +7,59 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+
 import { MatButtonModule } from '@angular/material/button';
-import { GunlistComponent } from './gunlist/gunlist.component';
+import { GunListComponent } from './gunlist/gunlist.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GunCardComponent } from './guncard/guncard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { GunModalComponent } from './gunmodal/gunmodal.component';
 
 @NgModule({
-  declarations: [AppComponent, GunlistComponent],
+  declarations: [
+    AppComponent,
+    GunListComponent,
+    GunCardComponent,
+    GunFormComponent,
+    GunModalComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatIconModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    MatSelectModule,
+    MatGridListModule,
+    MatInputModule,
+    MatDialogModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
